4b28c9a286ce1253985b8cec2cc01001f6eacf4b
rabauti <rabauti@gmail.com>
2017-07-03 10:37:05 +0300

    new controller for displaying search result

---
+++ app/controller/ExtendedSearchResultCtrl.js
@@ -0,0 +1,29 @@
+/**
+ * Created by katrin on 03.07.17.
+ */
+
+define([
+    'angularAMD',
+    'service/ExtendedSearchModalService'
+], function (angularAMD) {
+
+    angularAMD.controller('ExtendedSearchResultCtrl', ['$scope', 'service/ExtendedSearchModalService', function ($scope, extendedSearchModalService) {
+
+        console.log('ExtendedSearchResultCtrl');
+        var searchResult = ['init'];
+        
+      
+        
+        
+		$scope.init = function() {
+             
+             $scope.searchType = extendedSearchModalService.getSearchType(); 
+             $scope.searchResult = extendedSearchModalService.getSearchResult(); 
+			
+			
+		};
+        
+        $scope.init();
+		
+    }]);
+});
