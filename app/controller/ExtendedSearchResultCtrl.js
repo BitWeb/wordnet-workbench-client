d4a6a34935bdd27000e04a74ba9841e4413fa3b1
rabauti <rabauti@gmail.com>
2017-07-06 09:52:41 +0300

    search title added

--- app/controller/ExtendedSearchResultCtrl.js
+++ app/controller/ExtendedSearchResultCtrl.js
@@ -17,6 +17,7 @@ define([
         
 		$scope.init = function() {
              
+             $scope.searchTitle = extendedSearchModalService.getSearchTitle();
              $scope.searchType = extendedSearchModalService.getSearchType(); 
              $scope.searchResult = extendedSearchModalService.getSearchResult(); 
 			
