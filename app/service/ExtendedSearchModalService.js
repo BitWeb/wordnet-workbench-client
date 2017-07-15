832b777390c8c1762ed093b4d4bfac09353d331c
rabauti <rabauti@gmail.com>
2017-07-15 11:55:43 +0300

    lot of changes

--- app/service/ExtendedSearchModalService.js
+++ app/service/ExtendedSearchModalService.js
@@ -10,10 +10,21 @@ define([
         function($rootScope, $log, $sessionStorage, wnwbApi) {
             var self = this;
             var searchResult = [];
-            var searchType = 'Undefined';
+            var searchFilter = {};
+            var searchLimit = 50;
+            var searchType = 'synset';
             var searchTitle = 'Undefined';
-            
-            
+            var RootFilter = {
+                type: 'group',
+                boolOp: 'OR',
+                items : [ {type:'group', boolOp:'AND', items:[]}]
+                };
+
+            var filterTree = {};
+            filterTree.synset = angular.copy(RootFilter);
+            filterTree.sense = angular.copy(RootFilter);
+            filterTree.lexentry = angular.copy(RootFilter);
+
             var extendedSearchSession = {
                 isSaved:0,
                 filterRows: {
@@ -23,10 +34,9 @@ define([
                 },
                 searchType: 'synset'
             };
-            
-            
+
             this.init = function () {
-                if (!$sessionStorage.extendedSearchSession)
+                /*if (!$sessionStorage.extendedSearchSession)
                 {
                     $sessionStorage.extendedSearchSession = extendedSearchSession;
                     $sessionStorage.extendedSearchSession.isSaved = true;
@@ -34,8 +44,21 @@ define([
                 }
                 extendedSearchSession = $sessionStorage.extendedSearchSession;
                 
+                */
+                this.resetFilterTree();
             };
-            
+
+            this.resetFilterTree = function(searchtype = null) {
+                if (searchtype == null) {
+                    filterTree.synset = angular.copy(RootFilter);
+                    filterTree.sense = angular.copy(RootFilter);
+                    filterTree.lexentry = angular.copy(RootFilter);
+                }
+                else {
+                    filterTree[searchType] = angular.copy(RootFilter);
+                }
+            }
+
             this.isSavedFilter = function() {
                 return extendedSearchSession.isSaved;  
             }
@@ -49,6 +72,17 @@ define([
                  $sessionStorage.extendedSearchSession.searchType = searchType;
             };
             
+            this.getRootFilter = function () {
+                 return RootFilter;
+            }
+            
+            this.getFilterTree = function () {
+                 return filterTree;
+            }
+
+            this.setFilterTree = function (filtertree) {
+                filterTree = filtertree;
+            }
             this.getSearchFilterRows = function () {
                  return extendedSearchSession.filterRows;
             }
@@ -56,8 +90,8 @@ define([
                     return extendedSearchSession.searchType;
               
             };  
-            
-            
+
+            //FILTER SEARCH
             this.getLexentryFilterFieldsPromise = function(){  
                 return wnwbApi.LexicalEntrySearchOptions.query({}).$promise;
             };
@@ -69,13 +103,58 @@ define([
             this.getSynsetFilterFieldsPromise = function(){
                 return wnwbApi.SynsetSearchOptions.query({}).$promise;
             };
+
+            this.getLexEntrySearchPromise = function(filter, offset=0, limit=20){
+                var data = {};
+                data.filter = filter;
+                data.offset = offset;
+                data.limit = limit;
+                return wnwbApi.LexicalEntrySearch.query(data).$promise;
+            }
+
+            this.getSenseSearchPromise = function(filter, offset=0, limit=20){
+                var data = {};
+                data.filter = filter;
+                data.offset = offset;
+                data.limit = limit;
+                return wnwbApi.SenseSearch.query(data).$promise;
+            }
             
+            this.getSynsetSearchPromise = function(filter, offset=0, limit=20){
+                var data = {};
+                data.filter = filter;
+                data.offset = offset;
+                data.limit = limit;
+                return wnwbApi.SynsetSearch.query(data).$promise;
+            }
+
+            this.getSearchResultPromise = function(type, filter, offset=0, limit=20){
+               
+                if (type == 'lexentry') {
+                    return this.getLexEntrySearchPromise(filter,offset,limit);
+                }
+                else if (type == 'sense') {
+                    return this.getSenseSearchPromise(filter,offset,limit);
+                }
+                else  {
+                    return this.getSynsetSearchPromise(filter,offset,limit);
+                }
+            }
+             
             this.setSearchType = function(type){
                searchType = type;
             }
             
             this.getSearchType = function(){
                 return searchType;
+            }
+
+            this.setSearchQuery = function(query){
+               searchQuery = query;
+            }
+            
+            this.getSearchQuery = function(){
+                return searchQuery;
             }
 
             this.setSearchTitle = function(title){
@@ -86,6 +165,23 @@ define([
                 return searchTitle;
             }
 
+            this.setSearchLimit = function(limit){
+               searchLimit = limit;
+            }
+
+            this.getSearchLimit = function(){
+                return searchLimit;
+            }
+
+            this.setSearchFilter = function(filter){
+               searchFilter = filter;
+            }
+
+            this.getSearchFilter = function(filter){
+                return searchFilter;
+            }
+
+            
             this.setSearchResult = function(result){
                 searchResult = result;
                 $rootScope.$broadcast('newExtendedSearchResultChanged', result);
@@ -96,6 +192,6 @@ define([
             }
             
             self.init();
-            
+ 
         }]);
 });
