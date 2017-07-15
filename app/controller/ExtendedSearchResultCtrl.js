5745e22c8af5dd7eb65381cafa55c7381ca0b982
rabauti <rabauti@gmail.com>
2017-07-15 11:49:47 +0300

    pagination added

--- app/controller/ExtendedSearchResultCtrl.js
+++ app/controller/ExtendedSearchResultCtrl.js
@@ -7,27 +7,38 @@ define([
     'service/ExtendedSearchModalService'
 ], function (angularAMD) {
 
-    angularAMD.controller('ExtendedSearchResultCtrl', ['$scope', '$rootScope', 'service/ExtendedSearchModalService', function ($scope, $rootScope, extendedSearchModalService) {
+    angularAMD.controller('ExtendedSearchResultCtrl', ['$scope', '$rootScope', 'service/ExtendedSearchModalService', 'spinnerService', function ($scope, $rootScope, extendedSearchModalService, spinnerService) {
 
         console.log('ExtendedSearchResultCtrl');
         var searchResult = ['init'];
-        
-      
-        
+        $scope.currentPage = 1;
         
 		$scope.init = function() {
-             
              $scope.searchTitle = extendedSearchModalService.getSearchTitle();
              $scope.searchType = extendedSearchModalService.getSearchType(); 
              $scope.searchResult = extendedSearchModalService.getSearchResult(); 
-			
-			
+             $scope.searchLimit = extendedSearchModalService.getSearchLimit();
+		};
+
+        $scope.changePage = function(page) {
+            //console.log(page);
+            spinnerService.show('extSearchResultSpinner');
+            var offset = (page-1)*$scope.searchLimit;
+            var resultPromise = extendedSearchModalService.getSearchResultPromise(extendedSearchModalService.getSearchType(), extendedSearchModalService.getSearchFilter(), offset, $scope.searchLimit);
+            resultPromise.then(function(SearchResult) {
+                extendedSearchModalService.setSearchResult(SearchResult);
+                extendedSearchModalService.setSearchLimit($scope.searchLimit);
+                 spinnerService.hide('extSearchResultSpinner');
+            });
 		};
 
         $scope.init();
         
-        
-         $rootScope.$on('newExtendedSearchResultChanged', function (event) {
+        $scope.pageChanged = function() {
+             $scope.changePage($scope.currentPage);
+        };
+
+        $rootScope.$on('newExtendedSearchResultChanged', function (event) {
              console.log('newExtendedSearchResultChanged broadcast');
                $scope.init();
         });
